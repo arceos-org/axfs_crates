f05307f98aae78c1134494f5bf2d547ae952c115
ssy <879650736@qq.com>
2025-06-28 05:11:29 +0000

    add urandom

--- axfs_devfs/src/lib.rs
+++ axfs_devfs/src/lib.rs
@@ -8,6 +8,7 @@ extern crate alloc;
 
 mod dir;
 mod null;
+mod urandom;
 mod zero;
 
 #[cfg(test)]
@@ -15,6 +16,7 @@ mod tests;
 
 pub use self::dir::DirNode;
 pub use self::null::NullDev;
+pub use self::urandom::UrandomDev;
 pub use self::zero::ZeroDev;
 
 use alloc::sync::Arc;
